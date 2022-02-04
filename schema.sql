d55ec706bbe14da0bc3392d87184eb1ee877d405
Pau Rodriguez <pau@apothca.com>
2022-02-03 20:43:35 -0800

    added new tables, saved csv and query

--- schema.sql
+++ schema.sql
@@ -1,3 +1,25 @@
+---Drop table if exists ----
+DROP TABLE retirement_info;
+
+DROP TABLE current_emp;
+
+DROP TABLE emp_info;
+
+DROP TABLE retire_count;
+
+DROP TABLE departments;
+
+DROP TABLE employees;
+
+DROP TABLE dept_manager;
+
+DROP TABLE dept_emp;
+
+DROP TABLE salaries;
+
+DROP TABLE titles;
+
+
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
@@ -85,8 +107,6 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
 
-DROP TABLE retirement_info;
-
 -- Create new table for retiring employees
 SELECT emp_no, first_name, last_name
 INTO retirement_info
@@ -158,7 +178,6 @@ ORDER BY de.dept_no;
 
 
 Select * from retire_count;
-DROP TABLE retire_count;
 
 --- Per department counts---
 SELECT emp_no,
@@ -175,5 +194,74 @@ SELECT e.emp_no,
     e.first_name,
 e.last_name,
     e.gender,
-    s.salary,
+s.salary,
     de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+
+-- Check Table
+SELECT * FROM current_emp;
+
+-- List of managers per department
+SELECT dm.dept_no,
+	d.dept_name,
+	dm.emp_no,
+	ce.last_name,
+	dm.from_date,
+	dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+	INNER JOIN departments AS d
+		ON (dm.dept_no = d.dept_no)
+	INNER JOIN current_emp AS ce
+		ON (dm.emp_no = ce.emp_no);
+	
+SELECT * FROM manager_info;
+
+
+-- Join all 3 tables 
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+Into dept_info
+-- Def aliases with join and from statemnets
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+SELECT * FROM dept_info;
+
+----- Sales Team retire info
+SELECT di.emp_no,
+	di.first_name,
+	di.last_name,
+	di.dept_name
+INTO sales_dept
+FROM dept_info as di
+WHERE dept_name = 'Sales'
+ORDER BY first_name ASC, last_name DESC;
+
+SELECT * FROM sales_dept;
+
+
+SELECT di.emp_no,
+	di.first_name,
+	di.last_name,
+	di.dept_name
+INTO dev_sales_dept
+FROM dept_info as di
+WHERE dept_name in ('Sales', 'Development')
+ORDER BY dept_name, first_name ASC, last_name ASC;
+
+SELECT * FROM dev_sales_dept;
